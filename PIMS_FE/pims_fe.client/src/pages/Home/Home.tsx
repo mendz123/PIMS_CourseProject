26a9c8119a99593652763b4574704107ee93b0b7
phuclee <phucle682004@gmail.com>
2026-01-17 11:21:50 +0700

    cấu trúc lại trang home

---
+++ PIMS_FE/pims_fe.client/src/pages/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../../components/home/Navbar";
+import Hero from "../../components/home/Hero";
+import Features from "../../components/home/Features";
+import Timeline from "../../components/home/Timeline";
+import News from "../../components/home/News";
+import FAQ from "../../components/home/FAQ";
+import Footer from "../../components/home/Footer";
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <Timeline />
+        <News />
+        <FAQ />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
