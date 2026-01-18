f17d8aea2807e7bf001f3dd6ff25ae3d74800f83
phuclee <phucle682004@gmail.com>
2026-01-18 15:32:09 +0700

    login gg demo v1

--- PIMS_FE/pims_fe.client/src/App.tsx
+++ PIMS_FE/pims_fe.client/src/App.tsx
@@ -3,14 +3,17 @@ import { router } from "./routes";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import { NavigationProvider } from "./context/NavigationContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
   return (
-    <AuthProvider>
-      <NavigationProvider>
-        <RouterProvider router={router} />
-      </NavigationProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <NavigationProvider>
+          <RouterProvider router={router} />
+        </NavigationProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
