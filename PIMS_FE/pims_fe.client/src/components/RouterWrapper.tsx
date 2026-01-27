eb363a5961531f15ae8f29ae38583b95468c250d
phuclee <phucle682004@gmail.com>
2026-01-27 19:56:33 +0700

    remove loading bar

--- PIMS_FE/pims_fe.client/src/components/RouterWrapper.tsx
+++ PIMS_FE/pims_fe.client/src/components/RouterWrapper.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
-import TopLoadingBar from "./TopLoadingBar";
 
 const RouterWrapper = () => {
   return (
     <>
-      <TopLoadingBar />
       <Outlet />
     </>
   );
 };
 
 export default RouterWrapper;
-
-
