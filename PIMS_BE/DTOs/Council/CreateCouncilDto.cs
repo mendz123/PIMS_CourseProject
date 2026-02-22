038e7de5021aab3cfc83bf862d27b82c21ac4ac3
Nguyen Quang Khai <khaihoanxcc@gmail.com>
2026-02-22 19:02:36 +0700

    feat(council): add CreateCouncilDto with validation

---
+++ PIMS_BE/DTOs/Council/CreateCouncilDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PIMS_BE.DTOs.Council;
+
+public class CreateCouncilDto
+{
+    [Required(ErrorMessage = "Council name is required")]
+    [MaxLength(100)]
+    public string CouncilName { get; set; } = null!;
+
+    [Required(ErrorMessage = "SemesterId is required")]
+    public int SemesterId { get; set; }
+
+    /// <summary>List of UserId of teachers in the council</summary>
+    [Required]
+    [MinLength(1, ErrorMessage = "Council must have at least 1 member")]
+    public List<int> MemberUserIds { get; set; } = new();
+}
